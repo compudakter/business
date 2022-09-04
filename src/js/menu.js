c30502b7ce772c0c73577be4a2ff47fe1ba16c86
compudakter <zloycompudakter@gmail.com>
2022-09-04 16:11:15 +0300

    Add menu interaction

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,36 @@
-console.log("RUN==========");
-
+ 
 const toggler = document.querySelector(".menu__toggler");
-if (toggler) {
-  toggler.addEventListener("click", (e) => {
-    console.log("click");
-    const menuNav = document.querySelector(".menu__nav");
+const menuNav = document.querySelector(".menu__nav");
+
+if (toggler && menuNav) {
+  toggler.addEventListener("click", (e) => { 
     if (menuNav) {
       toggler.classList.toggle("_active");
       document.body.classList.toggle("_lock-overflow");
       menuNav.classList.toggle("_active");
     }
   });
+}
+
+const menuItems = document.querySelectorAll('.menu__item') 
+if(menuItems.length){
+  menuItems.forEach(mi=>{
+     
+    const sectionClass = mi.getAttribute('data-section') 
+    if(sectionClass){
+      mi.addEventListener('click',()=>{ 
+        const section = document.querySelector(sectionClass)
+        const headerHeight = document.querySelector('header').clientHeight
+        const top = section.getBoundingClientRect().top + scrollY - headerHeight
+        toggler.classList.remove("_active");
+        document.body.classList.remove("_lock-overflow");
+        menuNav.classList.remove("_active");
+        window.scrollTo({
+          top,
+          behavior:'smooth'
+        })
+      })
+    }
+     
+  })
 }
